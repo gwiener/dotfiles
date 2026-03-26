1c0959c6b3279b72c143e6a16a4e872cb11fc060
Guy Wiener <wiener.guy@gmail.com>
2026-03-26 13:13:16 +0200

    Add blink auto-completion

---
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -0,0 +1,43 @@
+return {
+  "saghen/blink.cmp",
+  version = "1.*",
+  dependencies = { "rafamadriz/friendly-snippets" },
+  opts = {
+    keymap = {
+      preset = "none",
+      ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+      ["<CR>"] = { "accept", "fallback" },
+      ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+      ["<C-e>"] = { "hide", "fallback" },
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+      ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+    },
+    appearance = {
+      nerd_font_variant = "mono",
+    },
+    completion = {
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+      ghost_text = {
+        enabled = true,
+      },
+      list = {
+        selection = {
+          preselect = true,
+          auto_insert = false,
+        },
+      },
+    },
+    sources = {
+      default = { "lsp", "snippets", "path", "buffer" },
+    },
+    signature = {
+      enabled = true,
+    },
+  },
+  opts_extend = { "sources.default" },
+}
