416876d46ddab4bc790d6fcaaf5a0946ddb88214
Guy Wiener <wiener.guy@gmail.com>
2026-04-05 10:52:52 +0300

    Tweak eza aliases for ignoring junk folders but not hidden ones

--- zsh/.config/zsh/plugins/eza.zsh
+++ zsh/.config/zsh/plugins/eza.zsh
@@ -1,4 +1,7 @@
-alias ll="eza -lF --icons --git"
-alias lt="eza -TF -L 4 --icons --ignore-glob '**/node_modules|**/.venv|**/*cache*'"
-alias la="eza -lF -a --icons --git"
-alias lg="eza -GF --icons --git"
+local eza_ignore='.git|.venv|__pycache__|node_modules|.mypy_cache|.pytest_cache|.ruff_cache|.tox|.eggs|*.egg-info|.DS_Store'
+
+alias l="eza -I '${eza_ignore}'"
+alias ll="eza -lF --icons --git -I '${eza_ignore}'"
+alias lt="eza -TF -L 4 --icons --follow-symlinks -I '${eza_ignore}'"
+alias la="eza -lF -a --icons --git -I '${eza_ignore}'"
+alias lg="eza -GF --icons --git -I '${eza_ignore}'"
