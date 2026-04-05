06a764e0d0d77c92177fd2e035a5ee97f0af320d
Guy Wiener <wiener.guy@gmail.com>
2026-04-05 10:55:11 +0300

    Tweak ZSH completions and fzf-tab to ignore junk folders but not hidden ones

--- zsh/.config/zsh/plugins/completions.zsh
+++ zsh/.config/zsh/plugins/completions.zsh
@@ -1,3 +1,10 @@
+# Show dotfiles in completion
+_comp_options+=(globdots)
+
+# Hide junk folders unless explicitly typed (_ignored retries without filter)
+zstyle ':completion:*' ignored-patterns '.git' '.venv' '__pycache__' 'node_modules' '.mypy_cache' '.pytest_cache' '.ruff_cache' '.tox' '.eggs' '*.egg-info' '.DS_Store'
+zstyle ':completion:*' completer _extensions _complete _ignored
+
 # fzf-tab: override shell completions with a nice pulldown
 source "/opt/homebrew/opt/fzf-tab/share/fzf-tab/fzf-tab.zsh"
 
