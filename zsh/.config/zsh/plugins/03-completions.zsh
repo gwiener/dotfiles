81266c6da0fdb9ad5699d2f6ca85a62d52694bb4
Guy Wiener <wiener.guy@gmail.com>
2026-04-06 15:31:06 +0300

    Enhanced completion styles

--- zsh/.config/zsh/plugins/03-completions.zsh
+++ zsh/.config/zsh/plugins/03-completions.zsh
@@ -4,8 +4,13 @@ _comp_options+=(globdots)
 # Hide junk folders unless explicitly typed (_ignored retries without filter)
 zstyle ':completion:*' ignored-patterns '.git' '.venv' '__pycache__' 'node_modules' '.mypy_cache' '.pytest_cache' '.ruff_cache' '.tox' '.eggs' '*.egg-info' '.DS_Store'
 zstyle ':completion:*' completer _extensions _complete _ignored
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:descriptions' format '[%d]'
+zstyle ':completion:*' menu no
 
 # fzf-tab: override shell completions with a nice pulldown
 source "/opt/homebrew/opt/fzf-tab/share/fzf-tab/fzf-tab.zsh"
+zstyle ':fzf-tab:*' continuous-trigger '/'
+zstyle ':fzf-tab:*' switch-group ',' '.'
 
 eval "$(uv generate-shell-completion zsh)"
