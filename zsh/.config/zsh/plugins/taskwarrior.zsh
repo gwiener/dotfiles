a62b7e8ed7cfd0d6819623d4d0f43c0724fc940c
Guy Wiener <wiener.guy@gmail.com>
2026-05-04 10:18:51 +0300

    Improved TaskWarrior filter completion with task ids and descriptions!

--- zsh/.config/zsh/plugins/taskwarrior.zsh
+++ zsh/.config/zsh/plugins/taskwarrior.zsh
@@ -1,3 +1,31 @@
+# Override _task_default to include ID completion (upstream has it commented out)
+_task_default() {
+  local cmd ret=1
+  integer i=1
+  local _task_cmds=($(task rc.hooks=0 _commands; task _aliases))
+  while (( i < $#words ))
+  do
+    cmd="${_task_cmds[(r)$words[$i]]}"
+    if (( $#cmd )); then
+      if (( $+functions[_task_${cmd}] )); then
+        _task_${cmd}
+        return ret
+      fi
+      _call_function ret _task_filter ||
+        _message "No command remaining."
+      return ret
+    fi
+    (( i++ ))
+  done
+
+  _task_subcommands
+  _task_ids
+  _task_aliases
+  _call_function ret _task_filter
+
+  return ret
+}
+
 prjt() {
   if [[ -L ".mind" ]]; then
     CURR=$(realpath .mind)
