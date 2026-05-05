33ebe4f8ff7aa804b5b2a95c885d7037ccfd805f
Guy Wiener <wiener.guy@gmail.com>
2026-05-05 09:56:22 +0300

    Local neovim file to show hidden files by default because of .config folders

---
+++ .nvim.lua
@@ -0,0 +1,21 @@
+-- Project-local config for DOTS: show dotfiles/hidden files in neo-tree and telescope
+-- (everything lives under .config/, so defaults hide most of the repo)
+
+require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+      hide_hidden = false,
+    }
+  }
+})
+
+require("telescope").setup({
+  defaults = {
+    hidden = true,
+  },
+  pickers = {
+    find_files = { hidden = true },
+    live_grep = { additional_args = { "--hidden" } },
+  }
+})
